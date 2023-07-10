2e3aca1726587080190d1249d4f8e458f560ed99
AliaaFathy <aliaaf518@gmail.com>
2023-07-10 16:03:33 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import React from "react";
 import ProfileCard from "./ProfileCard"
 import AlexaImages from  './images/alexa.png'
@@ -6,10 +7,43 @@ import SiriImages from  './images/siri.png'
 function App (){
     return(
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title="alexa" handle="@alexa99" imgSrc={AlexaImages}/>
-            <ProfileCard title="cortana" handle="@cortana32" imgSrc={CortanaImages}/>
-            <ProfileCard title="siri" handle="@siri01" imgSrc={SiriImages}/>
+            <section className="hero is-primary">
+            <div className="hero-body">
+                <p className="title">
+                    Personal Digital Assistants
+                </p>
+            </div>
+        </section>
+            <div className="container">
+                <div className="section">
+            <div className="columns">
+                <div className="column is-3">
+                    <ProfileCard
+                        title="alexa"
+                        handle="@alexa99"
+                        imgSrc={AlexaImages}
+                        description="Alexa was created by Amazon and helps you buy things"
+                    />
+                </div>
+                <div className="column is-3">
+                    <ProfileCard
+                        title="cortana"
+                        handle="@cortana32"
+                        imgSrc={CortanaImages}
+                        description="Cortana was made by Microsoft. who know what it does?"
+                    />
+                </div>
+                <div className="column is-3">
+                    <ProfileCard
+                        title="siri"
+                        handle="@siri01"
+                        imgSrc={SiriImages}
+                        description="Siri was made by Apple and is being phased out"
+                    />
+                </div>
+            </div>
+        </div>
+        </div>
         </div>
     );
 };
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,20 @@
-function ProfileCard({title,handle,imgSrc}){
+function ProfileCard({title,handle,imgSrc,description}){
     return (
-      <div>
-          <img src={imgSrc} alt ="pda logo" />
-          <div>title is {title} </div>
-          <div>handle is {handle} </div>
+      <div className="card">
+          <div className="card-image">
+              <figure className="image is-4by3">
+                <img src={imgSrc} alt ="pda logo" />
+              </figure>
+          </div>
+              <div className="card-content">
+                  <div className="media-content">
+                      <p className="title is-4">{title}</p>
+                      <p className="subtitle is-6">{handle}</p>
+                  </div>
+                  <div className="content">{description}
+                  </div>
+          </div>
+
       </div>
     );
 }
